781a8cf8a5b6e40a0fff7d78a662c52edd76d7ae
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2022-09-05 16:53:05 +0530

    create Firebase Project

---
+++ src/Firebase/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.React_App_apiKey as string,
+  authDomain: process.env.React_App_authDomain as string,
+  projectId: process.env.React_App_projectId as string,
+  storageBucket: process.env.React_app_storageBucket as string,
+  messagingSenderId: process.env.React_App_messagingSenderId as string,
+  appId: process.env.React_App_appId as string,
+};
+
+//const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+export const firestore = getFirestore();
+export const doc = collection(firestore, "userExManagement");
